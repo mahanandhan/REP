3d419c61fc1ef3d6b8349994e02f073daad36c19
mahanandhan <mahanandhanmanish@gmail.com>
2024-09-30 20:37:59 +0530

    p

---
+++ python2.py
@@ -0,0 +1,14 @@
+n = (input())
+a,b = n.split()
+a = int(a)
+b = int(b)
+sum = a+b
+diff = a-b
+mul = a*b
+div = a/b
+mod = a%b
+print("sum:",sum)
+print("diff:",diff)
+print("mul:",mul)
+print("div:",div)
+print("mod:",mod)
